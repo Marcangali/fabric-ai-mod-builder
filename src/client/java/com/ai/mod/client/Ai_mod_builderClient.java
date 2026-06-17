8e592e5517cc9b374ffaa8d157e475a2e2b19f02
Marcangali <80262834+Marcangali@users.noreply.github.com>
2026-06-17 17:25:23 +0200

    Add files via upload

---
+++ src/client/java/com/ai/mod/client/Ai_mod_builderClient.java
@@ -0,0 +1,10 @@
+package com.ai.mod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Ai_mod_builderClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
